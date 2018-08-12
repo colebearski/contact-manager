d89e883fde8f2c1d2d89f9d33da4b01dfb0e2518
colebearski <cole.pedersen@gmail.com>
2018-08-12 13:54:59 -0600

    23 changing state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,26 +5,48 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
-  state = {};
+  state = {
+    showContactInfo: false
+  };
 
-  onShowClick = e => {};
+  // onShowClick = e => {
+  //   // state is immutable, you can't directly manipulate it
+  //   // cannot do this.state = showContactInfo: false
+  //   // need to use setState with component based state
+  //   // !this = not
+  //      MOVED TO ARROW FUNCTION
+  //   this.setState({
+  //     showContactInfo: !this.state.showContactInfo
+  //   });
+  // };
 
   // binds the state to the function. This isn't a lifecycle function, it's custom
   // events in react
 
   render() {
     const { name, email, phone } = this.props.contact;
+    // destructure
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          {name}{" "}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+          />
           {/* this ^^ referring to a method in this class */}
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
+        {/* ? is ternary */}
       </div>
     );
   }
